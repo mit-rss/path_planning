d62278c10b63830c13f95c01a0123d7b9e487405
Caris Moses <carism@mit.edu>
2020-04-06 18:14:32 -0400

    making template for path planning and launch file

---
+++ src/path_planning.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from geometry_msgs.msg import PoseStamped, PoseArray, Point
+from nav_msgs.msg import Odometry, OccupancyGrid
+import rospkg
+import time, os
+from utils import LineTrajectory
+from PIL import Image
+from rrt import rrt_star
+
+class PathPlan(object):
+    """ Listens for goal pose published by RViz and uses it to plan a path from
+    current car pose.
+    """
+    def __init__(self):
+        self.odom_topic = rospy.get_param("~odom_topic")
+        self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_cb)
+        self.trajectory = LineTrajectory("/planned_trajectory")
+        self.goal_sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.goal_cb, queue_size=10)
+        self.traj_pub = rospy.Publisher("/trajectory/current", PoseArray, queue_size=10)
+        self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_cb)
+
+
+    def map_cb(self, msg):
+        pass ## REMOVE AND FILL IN ##
+
+
+    def odom_cb(self, msg):
+        pass ## REMOVE AND FILL IN ##
+
+
+    def goal_cb(self, msg):
+        pass ## REMOVE AND FILL IN ##
+
+
+if __name__=="__main__":
+    rospy.init_node("path_planning")
+    pf = PathPlan()
+    rospy.spin()
