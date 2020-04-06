076374b6baaccf0b957e103741defea35bff5457
Caris Moses <carism@mit.edu>
2020-04-06 18:05:19 -0400

    making fewer ros params in pure pursuit code so easier to plug in autograder

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -4,40 +4,36 @@ import rospy
 import numpy as np
 import time
 import utils
+import tf
 
-from geometry_msgs.msg import PolygonStamped
+from geometry_msgs.msg import PoseArray, PoseStamped
 from visualization_msgs.msg import Marker
-from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from ackermann_msgs.msg import AckermannDriveStamped
 from nav_msgs.msg import Odometry
 
 class PurePursuit(object):
-	""" Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
-	"""
-	def __init__(self):
-		self.trajectory_topic = rospy.get_param("~trajectory_topic")
-		self.odom_topic       = rospy.get_param("~odom_topic")
-		self.lookahead        = rospy.get_param("~lookahead")
-		self.speed            = float(rospy.get_param("~speed"))
-		self.wrap             = bool(rospy.get_param("~wrap"))
-		wheelbase_length      = float(rospy.get_param("~wheelbase"))
-		self.drive_topic      = rospy.get_param("~drive_topic")
+    """ Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
+    """
+    def __init__(self):
+        self.odom_topic       = rospy.get_param("~odom_topic")
+        self.lookahead        = # FILL IN #
+        self.speed            = # FILL IN #
+        self.wrap             = # FILL IN #
+        self.wheelbase_length = # FILL IN #
+        self.trajectory  = utils.LineTrajectory("/followed_trajectory")
+        self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
 
-		self.trajectory  = utils.LineTrajectory("/followed_trajectory")
-		self.traj_sub = rospy.Subscriber(self.trajectory_topic, PolygonStamped, self.trajectory_callback, queue_size=1)
 
-		'''
-		Insert code here
-		'''
+    def trajectory_callback(self, msg):
+        ''' Clears the currently followed trajectory, and loads the new one from the message
+        '''
+        print "Receiving new trajectory:", len(msg.poses), "points"
+        self.trajectory.clear()
+        self.trajectory.fromPoseArray(msg)
+        self.trajectory.publish_viz(duration=0.0)
 
-	def trajectory_callback(self, msg):
-		''' Clears the currently followed trajectory, and loads the new one from the message
-		'''
-		print "Receiving new trajectory:", len(msg.polygon.points), "points" 
-		self.trajectory.clear()
-		self.trajectory.fromPolygon(msg.polygon)
-		self.trajectory.publish_viz(duration=0.0)
 
 if __name__=="__main__":
-	rospy.init_node("pure_pursuit")
-	pf = PurePursuit()
-	rospy.spin()
+    rospy.init_node("pure_pursuit")
+    pf = PurePursuit()
+    rospy.spin()
